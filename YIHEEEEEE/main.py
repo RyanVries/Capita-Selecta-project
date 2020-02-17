26835f2ad3b7588b6f9c340d84e9c8505df4524b
Yi He Zhu <yihezhu@gmail.com>
2020-02-17 06:42:08 +0100

    update

--- YIHEEEEEE/main.py
+++ YIHEEEEEE/main.py
@@ -22,7 +22,7 @@ el = func.get_el(ELASTIX_PATH)
 # get images
 path = 'TrainingData'
 p_no_fix = 'p102'
-p_no_mov = 'p108'
+p_no_mov = 'p107'
 
 fix_im_p = os.path.join(path, p_no_fix, 'mr_bffe.mhd')
 fix_im = sitk.GetArrayFromImage(sitk.ReadImage(fix_im_p))
@@ -48,11 +48,11 @@ bspline64 = 'parameters_bspline64.txt'
 bspline32 = 'parameters_bspline32.txt'
 bspline16 = 'parameters_bspline16.txt'
 bspline8 = 'parameters_bspline8.txt'
-tr_im_p, tr_im_m_p = func.registration(rigid, fix_im_p, mov_im_p, mov_im_m_p, '_rigid')
-tr_im_p, tr_im_m_p = func.registration(bspline64, fix_im_p, tr_im_p, tr_im_m_p, '_bspline64')
-# tr_im_p, tr_im_m_p = func.registration(bspline32, fix_im_p, tr_im_p, tr_im_m_p, '_bspline32')
-# tr_im_p, tr_im_m_p = func.registration(bspline16, fix_im_p, tr_im_p, tr_im_m_p, '_bspline16')
-# tr_im_p, tr_im_m_p = func.registration(bspline8, fix_im_p, tr_im_p, tr_im_m_p, '_bspline8')
+#tr_im_p, tr_im_m_p = func.registration(rigid, fix_im_p, mov_im_p, mov_im_m_p, '_rigid')
+tr_im_p, tr_im_m_p = func.registration(bspline64, fix_im_p, mov_im_p, mov_im_m_p, '_bspline64')
+tr_im_p, tr_im_m_p = func.registration(bspline32, fix_im_p, tr_im_p, tr_im_m_p, '_bspline32')
+tr_im_p, tr_im_m_p = func.registration(bspline16, fix_im_p, tr_im_p, tr_im_m_p, '_bspline16')
+tr_im_p, tr_im_m_p = func.registration(bspline8, fix_im_p, tr_im_p, tr_im_m_p, '_bspline8')
 
 tr_im = sitk.GetArrayFromImage(sitk.ReadImage(tr_im_p))
 tr_im_m = sitk.GetArrayFromImage(sitk.ReadImage(tr_im_m_p))
